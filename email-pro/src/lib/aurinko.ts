a90484b25d3f359a6a2f18a41486dfc261e700be
TUSHAR292004 <tusharchandak2004@gmail.com>
2025-02-27 19:28:36 +0530

    EmailPro

--- email-pro/src/lib/aurinko.ts
+++ email-pro/src/lib/aurinko.ts
@@ -9,7 +9,7 @@ export const getAurinkoAuthUrl = async (serviceType: 'Google' | 'Office365') => 
     const params = new URLSearchParams({
         clientId: process.env.AURINKO_CLIENT_ID as string,
         serviceType,
-        scopes: "Mail.Read Mail.ReadWrite Mail.Send Mail.Draft Mail.All",
+        scopes : 'Mail.Read Mail.ReadWrite Mail.Send Mail.Drafts Mail.All',
         responseType: 'code',
         returnUrl: `${process.env.NEXT_PUBLIC_URL}/api/aurinko/callback`
     })
