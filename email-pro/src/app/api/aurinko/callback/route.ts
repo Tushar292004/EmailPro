b19fb027cb2b5286ddd3b41f8906bdada34257fb
TUSHAR292004 <tusharchandak2004@gmail.com>
2025-02-27 20:22:54 +0530

    EmailPro

--- email-pro/src/app/api/aurinko/callback/route.ts
+++ email-pro/src/app/api/aurinko/callback/route.ts
@@ -1,11 +1,25 @@
 // /api/aurinko/callback
 
+import { exchangeCodeForAccessToken, getAccountDetails } from "@/lib/aurinko";
 import { auth } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async (req: Request) => {
+export const GET = async (req: NextRequest) => {
     const {userId} = await auth()
-    console.log(userId);
-    return NextResponse.json({message: 'Hellow World'})
+    if (!userId) return NextResponse.json({message: 'Unauthroized'}, {status: 401})
+
+    const params = req.nextUrl.searchParams
+    const status = params.get('status')
+    if (status !+ 'success') return NextResponse.json({message: "Failed to link account"}, {status: 400})
+
+    // get the code to exchange for the access token    
+    const code = params.get('code')
+    if(!code) return NextResponse.json( {message: "No code provided"}, {status: 400} )
     
+    const token = await exchangeCodeForAccessToken(code)
+    if (!token) return NextResponse.json({message: "Failed to get exchange code for access token"}, {status: 400})
+
+    const accountDetails = await getAccountDetails(token.accessToken)
+
+    return NextResponse.json({message: 'Hello World'})
 }
--- email-pro/src/lib/aurinko.ts
+++ email-pro/src/lib/aurinko.ts
@@ -1,5 +1,5 @@
 "use server"
-
+import axios from 'axios'
 import { auth } from "@clerk/nextjs/server"
 
 export const getAurinkoAuthUrl = async (serviceType: 'Google' | 'Office365') => {
@@ -15,4 +15,47 @@ export const getAurinkoAuthUrl = async (serviceType: 'Google' | 'Office365') => 
     })
 
     return `https://api.aurinko.io/v1/auth/authorize?${params.toString()}`
+}
+
+export const exchangeCodeForAccessToken = async (code: string) => {
+    try {
+        const response = await axios.post('https://api.aurinko.io/v1/account', {}, {
+            auth: {
+                username: process.env.AURINKO_CLIENT_ID as string,
+                password: process.env.AURINKO_CLIENT_SECRET as string,
+            }
+        })
+
+        return response.data as { 
+            accountId: number,
+            accessToken: string,
+            userId: string,
+            userSession: string,
+        }
+    } catch (error) {
+        if (axios.isAxiosError(error)){
+            console.error(error.response?.data)
+        }
+        console.log(error)
+    }
+}
+
+export const getAccountDetails = async (accessToken: string) => {
+    try {
+        const response = await axios.get('https://api.aurinko.io/v1/account', {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            }
+        })
+
+        return response.data as {
+            email: string,
+            name: string,
+        }
+    } catch (error) {
+        if (axios.isAxiosError(error)){
+            console.error("Error fetching account details",error.response?.data)
+        }
+        console.log("Unexpected error fetching account details", error)
+    }
 }
